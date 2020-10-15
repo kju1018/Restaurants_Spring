3af59e001a9d3ddf69f1fe0aad0b91940200c6e5
KIM M S <kms951018@naver.com>
2020-10-15 15:46:34 +0900

    1.데이터 영속화 작업(H2 DateBase사용) 2.레스토랑 지역 정보를 추가하기 위해서 Region 클래스 추가

---
+++ eatgo-common/src/main/java/kr/co/fastcampus/eatgo/domain/Region.java
@@ -0,0 +1,22 @@
+package kr.co.fastcampus.eatgo.domain;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Region {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+}
---
+++ eatgo-common/src/test/java/kr/co/fastcampus/eatgo/domain/RegionTest.java
@@ -0,0 +1,15 @@
+package kr.co.fastcampus.eatgo.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RegionTest {
+
+    @Test
+    public void creation(){
+        Region region = Region.builder().name("서울").build();
+
+        assertThat(region.getName()).isEqualTo("서울");
+    }
+}
